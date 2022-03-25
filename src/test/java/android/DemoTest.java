b4ae1678d1f14bdcc8bdc8479db5051081a013ca
Mathew Kuruvila <kuruvila.mathew@gmail.com>
2022-03-26 10:39:46 +1100

    Changes

---
+++ src/test/java/android/DemoTest.java
@@ -0,0 +1,28 @@
+package android;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void demoTest1() {
+		System.out.println("DemoTest 1");
+	}
+	
+	@Test
+	public void demoTest2() {
+		System.out.println("DemoTest 2");
+	}
+	
+	
+	@Test
+	public void demoTest3() {
+		System.out.println("DemoTest 3");
+	}
+	
+	
+	@Test
+	public void demoTest4() {
+		System.out.println("DemoTest 4");
+	}
+}
