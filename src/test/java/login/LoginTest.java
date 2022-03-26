d6ef23759d43e08929a90b9bd6402fb1fbbc7fa1
Mathew Kuruvila <kuruvila.mathew@gmail.com>
2022-03-26 17:46:57 +1100

    Changes

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 
 public class LoginTest {
 
@@ -11,23 +12,32 @@ public class LoginTest {
 	@Description("Test Description: Login test with wrong username and wrong password. loginDemoTest1")
 	public void loginDemoTest1() {
 		System.out.println("loginDemoTest1");
+		Assert.fail("Failed the test");
 	}
 	
 	@Test
 	public void loginDemoTest2() {
 		System.out.println("DemoTest 2");
+		login();
 	}
 	
 	
 	@Test
 	public void loginDemoTest3() {
 		System.out.println("DemoTest 3");
-		Assert.fail("Failed the test");
+		
 	}
 	
 	
 	@Test
 	public void loginDemoTest4() {
 		System.out.println("DemoTest 4");
+		login();
+	}
+	
+	@Step("This is the a new step")
+	public void login() {
+		System.out.println("Login");
+		Assert.fail("Failed the test");
 	}
 }
--- src/test/java/welcome/DemoTest.java
+++ src/test/java/welcome/DemoTest.java
@@ -12,7 +12,7 @@ public class DemoTest {
 	@Description("Test Description: Login test with wrong username and wrong password. demoTest1")
 	public void demoTest1() {
 		System.out.println("DemoTest 1");
-		Assert.fail("Failed the test");
+		login();
 	}
 	
 	@Test
@@ -20,17 +20,26 @@ public class DemoTest {
 	public void demoTest2() {
 		System.out.println("DemoTest 2");
 		login();
+		loginAnother();
 	}
 	
 	@Step("This is the login step")
 	public void login() {
 		System.out.println("Login");
+		Assert.fail("Failed the test");
+	}
+	
+	@Step("This is another login step")
+	public void loginAnother() {
+		System.out.println("Login");
+		Assert.fail("Failed the test");
 	}
 	
 	@Test
 	@Description("Test Description: Login test with wrong username and wrong password. demoTest3")
 	public void demoTest3() {
 		System.out.println("DemoTest 3");
+		loginAnother();
 		Assert.fail("Failed the test");
 	}
 	
@@ -39,5 +48,6 @@ public class DemoTest {
 	@Description("Test Description: Login test with wrong username and wrong password. demoTest4")
 	public void demoTest4() {
 		System.out.println("DemoTest 4");
+		login();
 	}
 }
